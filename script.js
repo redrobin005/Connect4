36d7b58a1b9796515c9e12b3bafd09f7b0812df6
anwin <arobin105@gmail.com>
2022-04-25 18:20:19 +0100

    Chips now drawn onto board when column is clicked

--- script.js
+++ script.js
@@ -23,11 +23,27 @@ let turnCount = 0
 function dropChip(){
     let colNum = this.parentElement.getAttribute("col-num")
     colNum = parseInt(colNum)
-    if (turnCount % 2 === 0) {
-        grid[colNum].push('r')
-    }else{
-        grid[colNum].push('y')
+    if (grid[colNum].length < 6) {
+        if (turnCount % 2 === 0) {
+            grid[colNum].push('r')
+        }else{
+            grid[colNum].push('y')
+        }  
     }
     ++turnCount;
-    console.log(grid)
+    drawBoard()
+}
+
+function drawBoard(){
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            let colElement = document.querySelectorAll(`[col-num="${i}"]`)[0]
+            let cellElement = colElement.querySelectorAll(`[row-num="${j}"]`)[0]
+            if (grid[i][j] === 'r') {
+                cellElement.style.backgroundColor  = 'red'
+            }else{
+                cellElement.style.backgroundColor  = 'yellow'
+            }
+        }
+    }
 }
