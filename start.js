2194373a7c903d193c9b6f75440ad3742571b92b
anwin <arobin105@gmail.com>
2022-05-06 18:37:28 +0100

    Random AI

--- start.js
+++ start.js
@@ -1,4 +1,4 @@
-function setGame(){
+function setGame(randomAI){
     let rowSize = document.querySelector('#rowSize').value
     let colSize = document.querySelector('#colSize').value 
     let playerOne = document.querySelector('#playerOne').value
@@ -8,6 +8,9 @@ function setGame(){
     sessionStorage.setItem('colSize', colSize)
     sessionStorage.setItem('playerOne', playerOne)
     sessionStorage.setItem('playerTwo', playerTwo)
+    
+    sessionStorage.setItem('randomAI', randomAI)
+    console.log(randomAI)
     
     window.location.href = "index.html"
 }
