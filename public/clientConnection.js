da1929dea341aafd51d3796a80530445825edd36
anwin <arobin105@gmail.com>
2022-05-09 19:05:38 +0100

    Player connections now recognised

--- public/clientConnection.js
+++ public/clientConnection.js
@@ -1,1 +1,26 @@
+let currentPlayer = "user"
+let playerNum = 0
+let ready = false
+let enemyReady = false
+
 const socket = io()
+
+// get player num (which was emitted in server.js)
+socket.on('player-number', num => {
+    infoDisplay = document.querySelector('#infoDisplay')
+    if (num === -1) {
+        infoDisplay.innerHTML = "Sorry server is full"
+    }else{
+        playerNum = parseInt(num)
+        if (playerNum === 1) currentPlayer = "enemy"
+
+        console.log('player num', playerNum)
+        // playerConnectedOrDisconnected(playerNum)
+    }
+
+})
+
+// another player has connected/disconnected (on player 2 connection)
+socket.on('player-connection', num =>{
+    console.log(`Player ${num} has connected or disconnected`)
+})
--- server.js
+++ server.js
@@ -11,9 +11,32 @@ const io = socketio(server)
 app.use(express.static(path.join(__dirname, "public"), {index:'start.html'}))
 
 // start server
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
+const connections = [null, null]
 // handle new socket connection from client
 io.on('connection', socket => {
     console.log('New WS connection')
+
+    // find available player number connection (one that isn't null)
+    let playerIndex = -1
+    for(const i in connections){
+        if(connections[i] === null){
+            playerIndex = i
+            break
+        }
+    }
+
+    // tell (attempted) connecting client their player num
+    socket.emit('player-number', playerIndex)
+    console.log(`Player ${playerIndex} has connected`)
+
+    // ignore player 3 and beyond
+    if (playerIndex === -1) return
+
+    // if player connected but not ready, then null to false
+    connections[playerIndex] = false
+
+    // tell everyone the player num that has actually connected
+    socket.broadcast.emit('player-connection', playerIndex)
 })
